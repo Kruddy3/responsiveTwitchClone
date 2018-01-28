0e93a7d6f2f71368518f21ee90d070783ee41475
thestraded <thestraded@gmail.com>
2018-01-27 19:03:15 -0500

    Working dual scroll

---
+++ index.js
@@ -0,0 +1,12 @@
+$(function(){
+
+  $('.linked-scrollbar-fixed').attr("data-scrolling", "false");
+
+  $('.linked-scrollbar-fixed').scroll(function(){
+      if($(this).attr("data-scrolling") == "false"){
+          $('.linked-scrollbar-fixed').not(this).attr("data-scrolling", "true");
+          $('.linked-scrollbar-fixed').not(this).scrollTop($(this).scrollTop());
+      }
+      $(this).attr("data-scrolling", "false");
+  });
+})
